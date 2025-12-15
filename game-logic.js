35c5f05481cb0ab021bc84208b9e380719e140c2
Haippp <haiperdesain@gmail.com>
2025-12-16 01:07:13 +0800

    tadi ada bug cuy

--- game-logic.js
+++ game-logic.js
@@ -59,15 +59,14 @@ function checkAnswer() {
                 alert.innerHTML = 'Jawaban anda benar!'
                 alert.className = 'mt-3 rounded-md bg-green-100 text-green-800 px-4 py-2 text-sm text-center'
                 setTimeout(()=> alert.classList.add('hidden'), 2000)
+                if (level === 6){
+                    showModal()
+                }
                 loadLevel()
             } else {
                 alert.innerHTML = 'Jawaban anda salah!'
                 alert.className = 'mt-3 rounded-md bg-red-100 text-red-800 px-4 py-2 text-sm text-center'
                 setTimeout(()=> alert.classList.add('hidden'), 2000)
-            }
-            
-            if (level === 6){
-                showModal()
             }
         })
     })

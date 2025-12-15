47023e488d6c34b31ded80be806fb7fef2749f6d
Haippp <haiperdesain@gmail.com>
2025-12-16 00:59:06 +0800

    sedikit perbaikan

--- game-logic.js
+++ game-logic.js
@@ -1,9 +1,13 @@
-
 let level = Number(localStorage.getItem('level')) ? Number(localStorage.getItem('level')) : 1
 const title = document.getElementById('title')
 const desc = document.getElementById('description')
 const ct = document.getElementById('ciphertext')
 const userInput = document.getElementById('answer')
+document.getElementById('answer').addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      checkAnswer()
+    }
+})
 
 function getData(lvl) {
     return fetch('./content.json').then(res => res.json())
@@ -23,13 +27,24 @@ async function sha256(text) {
 }
 
 function loadLevel(){
-    getData(level).then(data => {
-    title.innerText = data.title
-    desc.innerText = data.desc
-    ct.innerText = data.cipher
-})
-document.title = `Level ${level}`
-localStorage.setItem('level', level)
+    if (level <= 5){
+        getData(level).then(data => {
+        title.innerText = data.title
+        desc.innerText = data.desc
+        ct.innerText = data.cipher
+        })
+    } else 
+        level = 1
+    document.title = `Level ${level}`
+    localStorage.setItem('level', level)
+}
+
+function showModal() {
+  document.getElementById('finishModal').classList.remove('hidden')
+}
+
+function closeModal() {
+  document.getElementById('finishModal').classList.add('hidden')
 }
 
 function checkAnswer() {
@@ -41,15 +56,19 @@ function checkAnswer() {
             userInput.value = ''
             if(isCorrect){
                 level++
-                loadLevel()
                 alert.innerHTML = 'Jawaban anda benar!'
                 alert.className = 'mt-3 rounded-md bg-green-100 text-green-800 px-4 py-2 text-sm text-center'
+                setTimeout(()=> alert.classList.add('hidden'), 2000)
+                loadLevel()
             } else {
                 alert.innerHTML = 'Jawaban anda salah!'
                 alert.className = 'mt-3 rounded-md bg-red-100 text-red-800 px-4 py-2 text-sm text-center'
-                
+                setTimeout(()=> alert.classList.add('hidden'), 2000)
             }
-            setTimeout(()=> alert.classList.add('hidden'), 1000)
+            
+            if (level === 6){
+                showModal()
+            }
         })
     })
 }
